0ee727bbeb60a592613189395845844db11840e1
Lu Liang <lu@LudeMacBook-Pro.local>
2025-11-19 17:38:35 +0000

    wls function completed; plot function updated.

--- scripts/plot_ts_tidy_tidy.py
+++ scripts/plot_ts_tidy_tidy.py
@@ -8,6 +8,7 @@ Update: 28 Oct 2025 - plot ts of cum and ground water for specific well
 Update: 6  Nov 2025 - output plots path added.
 Update: 15 Nov 2025 - WLS fitting function added; plot function updated to show gw AND cum.
 Update: 17 Nov 2025 - plot functions updated to avoid duplicate loops.
+Update: 19 Nov 2025 - WLS fitting function applied and export to csv; plot_ts function updated to add plots of wls results.
 '''
 
 import os 
@@ -69,16 +70,22 @@ def calc_wls (x, y, eps=1e-8):
     Cite: https://www.statsmodels.org/dev/generated/statsmodels.regression.linear_model.OLS.html
     '''
     # clear data (nan will cause error)
-    mask = ~np.isnan(x) & ~np.isnan(y)
+    x = np.asarray(x, float)
+    y = np.asarray(y, float)
+
+    mask = np.isfinite(x) & np.isfinite(y)
     x = x[mask]
     y = y[mask]
+    
+    # skip nan or insufficient data
+    if x.size < 2:
+        return None
 
     # Apply OLS first
     x_const = sm.add_constant(x)
     ols_model = sm.OLS(y, x_const)
     ols_result = ols_model.fit()
     # print(ols_result.summary())
-    print(ols_result.t_test([1, 0]))
 
     # Apply 1st WLS
     abs_res = np.abs(ols_result.resid)                    # absolute residuals: |observed y - fitted y|
@@ -90,7 +97,6 @@ def calc_wls (x, y, eps=1e-8):
     weight1 = 1.0 / (pred_var ** 2)                       # weights: w = 1 / variance^2
     wls1_result = sm.WLS(y, x_const, weights=weight1).fit()
     # print(wls1_result.summary())
-    print(wls1_result.t_test([1, 0]))
 
     # Apply 2nd WLS
     abs_res2 = np.abs(wls1_result.resid)
@@ -100,63 +106,70 @@ def calc_wls (x, y, eps=1e-8):
     wt2 = 1.0 / (pred_var2 ** 2)
     wls2_result = sm.WLS(y, x_const, weights=wt2).fit()
     # print(wls2_result.summary())
-    print(wls2_result.t_test([1, 0]))
 
-    # return wls2_fit
+    # print(f'WLS slope: {wls2_result.params[1]:.6f} +/- {wls2_result.bse[1]:.6f}')
+    return wls2_result
 
 
-def plot_ts(df, lon_idx, lat_idx, cum, dt, frame_base):
+def plot_ts(gw_df, cum_ts, cum_dt, wid, frame_base, 
+            gw_model=None, gw_x=None, 
+            cum_model=None, cum_x=None):
     '''
     Function to plot groundwater and cum displacement ts
     of coords that located within each hdf5 files
     '''
-    groups = df.groupby('well_id')
-    # print(groups.groups.keys())
-    plotted = 0
-    # select cum form choosen lon/lat_idx
-    for wid, xi, yi in zip(groups.groups.keys(), lon_idx, lat_idx):    # zip() to iterate two lists together
-        # print(wid, xi, yi)
-        # xi, yi = int(xi), int(yi)
-        cum_ts = cum[:, yi, xi].astype(float)
-        if not np.isfinite(cum_ts).any():
-            continue
-        sub = groups.get_group(wid).sort_values('date')
 
-        # plot
-        fig, ax = plt.subplots(figsize=(12, 7), dpi=50)
-        ax2 = ax.twinx()
+    if not np.isfinite(cum_ts).any():
+        return False
 
-        # plot groundwater level
-        sns.scatterplot(
-            x=sub['date'], y=sub['obs_gw'], ax=ax, 
-            s=50, alpha=0.5, facecolor='steelblue', edgecolor='navy', linewidth=0.8,
-            label='Ground Water Level', legend=False
-        )
+    # plot
+    fig, ax = plt.subplots(figsize=(12, 7), dpi=50)
+    ax2 = ax.twinx()
 
-        # plot cummulative displacement
-        sns.scatterplot(
-            x=dt, y=cum_ts, ax=ax2, 
-            s=50, alpha=0.5, facecolor='grey', edgecolor='dimgray', linewidth=0.8,
-            label='Cum displacement', legend=False
-        )
+    # plot groundwater level
+    sns.scatterplot(
+        x=gw_df['date'], y=gw_df['obs_gw'], ax=ax, 
+        s=50, alpha=0.5, facecolor='steelblue', edgecolor='navy', linewidth=0.8,
+        label='Ground Water Level', legend=False
+    )
 
-        # ax settings
-        ax.set_title(f'Well ID: {wid} Frame: {frame_base}', fontsize=12)
-        ax.set_xlabel('Date', fontsize=12)
-        ax.set_ylabel('Ground Water Level (m)', fontsize=12)
-        ax2.set_ylabel('LOS Displacement (mm)', fontsize=12)
-        ax.grid(linestyle='--', alpha=0.5, color='steelblue')
-        ax.relim()
-        ax.autoscale()
-        # keep 2 decimal places 
-        ax.ticklabel_format(style='plain', axis='y', useOffset=False)
-        ax.yaxis.set_major_formatter(mtick.FormatStrFormatter('%.2f'))
+    # plot cummulative displacement
+    sns.scatterplot(
+        x=cum_dt, y=cum_ts, ax=ax2, 
+        s=50, alpha=0.5, facecolor='grey', edgecolor='dimgray', linewidth=0.8,
+        label='Cum displacement', legend=False
+    )
 
-        plt.savefig(os.path.join(OUT_DIR, f'F{frame_base}_W{wid}.png'))
-        # plt.show()
-        # plt.close(fig)
-        plotted += 1
-    print(f'Total {plotted} out of {len(groups.groups.keys())} wells plotted for frame {frame_base}.')
+    # plot wls result
+    # gw
+    if (gw_model is not None) and (gw_x is not None):
+        gw_y_fit = gw_model.predict(sm.add_constant(gw_x))
+        ax.plot(gw_df['date'], gw_y_fit, color='darkblue', linestyle='--', linewidth=1.5)
+    # cum
+    if (cum_model is not None) and (cum_x is not None):
+        cum_y_fit = cum_model.predict(sm.add_constant(cum_x))
+        ax2.plot(cum_dt, cum_y_fit, color='black', linestyle='--', linewidth=1.5)
+
+
+    # ax settings
+    ax.set_title(f'Well ID: {wid} Frame: {frame_base}', fontsize=12)
+    ax.set_xlabel('Date', fontsize=12)
+    ax.set_ylabel('Ground Water Level (m)', fontsize=12)
+    ax2.set_ylabel('LOS Displacement (mm)', fontsize=12)
+    ax.grid(linestyle='--', alpha=0.5, color='steelblue')
+
+    ymin, ymax = ax.get_ylim()
+    yrange = ymax - ymin
+    ax.set_ylim(ymin - 0.05*yrange, ymax + 0.05*yrange)
+    # keep 2 decimal places 
+    ax.ticklabel_format(style='plain', axis='y', useOffset=False)
+    ax.yaxis.set_major_formatter(mtick.FormatStrFormatter('%.2f'))
+
+    plt.savefig(os.path.join(OUT_DIR, f'F{frame_base}_W{wid}.png'))
+    # plt.show()
+    plt.close(fig)
+
+    return True
 
 
 
@@ -207,55 +220,96 @@ if __name__ == '__main__':
     # print(df.iloc[:11, :])
     print(f'CSV data loaded successfully.')
 
-
-    # 2) load hdf5 files
+    # 2) Wells and groups
     # 2.1 select all wells, and avoid duplicates
     wells = df[['well_id', 'lon', 'lat']].drop_duplicates('well_id')
     print(f'Total {wells.shape[0]} wells found in CSV.')
+    
+    # group by well_id
+    groups = df.groupby('well_id')
 
-    # 2.2 get h5 filenames
+    # create a container for wls results
+    wls_results = []
+
+
+    # 3) load hdf5 files
+    # 3.1 get h5 filenames
     h5_fn = sorted(glob.glob(IN_H5))
     print(f'Total {len(h5_fn)} hdf5 found.')
 
-    # 2.3 read in h5
+    # 3.2 loop for each hdf5 file
     for fn in h5_fn:
         frame_base = os.path.basename(fn).split('.cum_filt.h5')[0]
         print(f'Processing {frame_base} ...')
 
-        f = h5.File(fn, 'r')
+        frame_plotted = 0
 
-        # get lon and lat values
-        lon = get_dim(f, 'lon')
-        lat = get_dim(f, 'lat')
+        # read in hdf5 file
+        with h5.File(fn, 'r') as f:
 
-        # get vel values
-        cum = f['cum'][:]
+            # get lon and lat values
+            lon = get_dim(f, 'lon')
+            lat = get_dim(f, 'lat')
 
-        # get imdates
-        imdates = f['imdates'][:]
-        dt = [datetime.datetime.strptime(str(date), "%Y%m%d") for date in imdates]
+            # get vel values
+            cum = f['cum'][:]
 
-        # find closest index for each well
-        lon_idx = [find_closest_index(lon, lon_x) for lon_x in wells['lon']]
-        lat_idx = [find_closest_index(lat, lat_x) for lat_x in wells['lat']]
-        # print(df['lon'][:5])
-        # print(lon_idx[:5])
-        # print(df['lat'][:5])
-        # print(lat_idx[:5])
+            # get imdates
+            imdates = f['imdates'][:]
+            cum_dates = [datetime.datetime.strptime(str(d), "%Y%m%d") for d in imdates]
 
+            # find closest index for each well
+            lon_idx = [find_closest_index(lon, lon_x) for lon_x in wells['lon']]
+            lat_idx = [find_closest_index(lat, lat_x) for lat_x in wells['lat']]
+            # print(df['lon'][:5])
+            # print(lon_idx[:5])
+            # print(df['lat'][:5])
+            # print(lat_idx[:5])
 
+            # 4) WLS fitting
+            # 4.1 loop each well within this frame
+            for wid, xi, yi, lon, lat in zip(wells['well_id'], lon_idx, lat_idx, wells['lon'], wells['lat']):    # zip() to iterate two lists together
 
-    # # 3) WLS model
-    # # 3.1 WLS for obs_gw vs date
-    # # for each wells
-    # sub = df[df['well_id'] == 'wid'].sort_values('date')
-    # # sub = df[df['well_id'] == '620302210010'].sort_values('date') 
-    # x_gw = (sub['date'] - sub['date'].min()).dt.days.values.astype(float)
-    # y_gw = sub['obs_gw'].values.astype(float)
-    # print(f'Well {'well_id'}: x size = {x_gw.size}, y size = {y_gw.size}')
+                # 4.2 groundwater WLS
+                sub = groups.get_group(wid).sort_values('date') # get date for specific well
+                gw_ts = sub['obs_gw'].values.astype(float)
+                gw_x = (sub['date'] - sub['date'].min()).dt.days.values.astype(float)
+                # print(f'Well {wid}: x size = {x_gw.size}, y size = {y_gw.size}')
+                gw_model = calc_wls(gw_x, gw_ts)
+                gw_vel, gw_unc = gw_model.params[1], gw_model.bse[1]
 
-    # calc_wls(x_gw, y_gw)
-    
+                # 4.3 cum WLS
+                xi, yi = int(xi), int(yi)
+                cum_ts = cum[:, yi, xi].astype(float)
+                cum_x = np.array([(d - cum_dates[0]).days for d in cum_dates], dtype=float)
+                cum_model = calc_wls(cum_x, cum_ts)
+                if cum_model is not None:
+                    cum_vel, cum_unc = cum_model.params[1], cum_model.bse[1]
+                else:
+                    continue
 
-    plot_ts(df, lon_idx, lat_idx, cum, dt, frame_base)
-        
+                # 4.4 store reults
+                wls_results.append({
+                    'frame': frame_base,
+                    'well_id': wid,
+                    'lon': lon,
+                    'lat': lat,
+                    'gw_vel': gw_vel,
+                    'gw_unc': gw_unc,
+                    'cum_vel': cum_vel,
+                    'cum_unc': cum_unc
+                })
+
+                # 5) plot
+                plotted = plot_ts(sub, cum_ts, cum_dates, wid, frame_base, gw_model, gw_x, cum_model, cum_x)
+                if plotted:
+                    frame_plotted += 1 
+
+        print(f'Frame {frame_base}: plotted {frame_plotted} / {len(wells)} wells.')
+
+    # 4.5 export wls results to csv
+    wls_pd = pd.DataFrame(wls_results)
+    out_csv = os.path.join(BASE_DIR, 'data', 'gw_cum_wls.csv')
+    wls_pd.to_csv(out_csv, index=False)
+    print(f'Output csv saved to {out_csv}.')
+    print('Finished')
