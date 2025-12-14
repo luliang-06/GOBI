cb037f0a40a014f51583e04f38db5d5d806e84ca
Lu Liang <lu@LudeMacBook-Pro.local>
2025-12-14 00:36:52 +0000

    coord extent updated

--- scripts/plot_points_on_raster.sh
+++ scripts/plot_points_on_raster.sh
@@ -11,15 +11,17 @@ gmt set MAP_LABEL_OFFSET 2p
 gmt set MAP_ANNOT_OFFSET_SECONDARY auto
 
 
-raster=/exports/geos.ed.ac.uk/comet/lliang/GOBI_proj/outputs/gw_vel_all.nc # path to a raster file
+# raster=/exports/geos.ed.ac.uk/comet/lliang/GOBI_proj/outputs/gw_vel_all.nc # path to a raster file
+raster=/Users/lu/Documents/data/vu_John_new.grd
 
 # echo "lon lat rate 
 # 70 40 1.2
 # 80 48 -2.1" > points.txt 
-points=/exports/geos.ed.ac.uk/comet/lliang/GOBI_proj/data/gw_cum_wls.csv # or a separate txt file (if comma delimited, you'll need to change the awk statement below slightly)
+# points=/exports/geos.ed.ac.uk/comet/lliang/GOBI_proj/data/gw_cum_wls.csv # or a separate txt file (if comma delimited, you'll need to change the awk statement below slightly)
+points=/Users/lu/Documents/data/gw_cum_wls_ALL.csv
 
 gmt begin points_on_raster png,pdf
-	gmt basemap -R101.5/104.3/37/39.3 -JX6i -B1 -BWeSn  # draw the map frame, change the boundary to -Rleft/right/bottom/top -B5 for a grid tick every 5 degrees, play with how you capitalise wesn after the second -B
+	gmt basemap -R101.7/104.7/37.3/39.3 -JX6i -B1 -BWeSn  # draw the map frame, change the boundary to -Rleft/right/bottom/top -B5 for a grid tick every 5 degrees, play with how you capitalise wesn after the second -B
     gmt makecpt -Croma -T-1/1 -I # change the limits of your colour map
 	gmt grdimage $raster -n+c -Q 
     gmt colorbar -DjTL+w1.8/13%+o1/1+h+e+ml -Bx1+l"Vu, mm/yr"  # change the 3 in -Bx3 for tick label interval on the colourbar
