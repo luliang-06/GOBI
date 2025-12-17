471a15e38984391e4abc97de787e5cd9be5eea60
luliang-06 <s2593484@forth03.geos.ed.ac.uk>
2025-12-17 17:14:24 +0000

    plotting added

--- scripts/plot_reg.py
+++ scripts/plot_reg.py
@@ -8,15 +8,18 @@ Outputs: GW_vel_VU.png
 
 import os 
 import numpy as np
+import pandas as pd
 import seaborn as sns
 import geopandas as gpd
 import matplotlib.pyplot as plt
-from shapely.geometry import Point
+import statsmodels.api as sm
+
+from plot_ts import load_gw_obs_csv, calc_wls
 from gps_reference import OpenTif
 
 
 BASE_DIR = os.path.abspath(os.path.join(os.path.dirname(__file__), '..', '..'))
-IN_GW = os.path.join(BASE_DIR, 'data', 'gw_cum_wls_FIXED.csv')
+IN_GW = os.path.join(BASE_DIR, 'data', '2018-2022_ShiyangBasin_Groundwater_WaterLevel_FIXED.csv')
 IN_VU = os.path.join(BASE_DIR, 'data', 'vu_shiyang_referenced.tif')
 OUT_DIR = os.path.join(BASE_DIR, 'outputs')
 os.makedirs(OUT_DIR, exist_ok=True)   
@@ -28,26 +31,79 @@ plt.rcParams['font.sans-serif'] = ['DejaVu Sans']
 plt.rcParams['xtick.labelsize'] = 12
 plt.rcParams['ytick.labelsize'] = 12
 
-def load_gdf(in_file):
-    gdf = gpd.GeoDataFrame(geometry=[], crs="EPSG:4326")
-    gdf['geometry'] = None
-    index = 0
-    fl = open(in_file, "r").readlines()
-    for line in fl:
-        frame, well_id, lon, lat, gw_vel, gw_unc, cum_vel, cum_unc = line.split()
-        gdf.loc[index, 'geometry'] = Point(float(lon), float(lat))
-        gdf.loc[index, 'well_id'] = well_id[:]
-     
 
-if __name__ == '__main__':
-    # read in gw table
-    gw_gdf = load_gdf(IN_GW)
-    
-    # Open vu tif
-    vu_tif = OpenTif(IN_VU)
-    
-    # extract vu value at well location
-    vu, vu_sd = [vu_tif.extract_pixel_value(point.x, point.y, 8)[0] for point in gw_gdf['geometry']]
-    gw_gdf.dropna(inplace=True)
 
-    print(gw_gdf.head)
+gw_df = load_gw_obs_csv(IN_GW)
+# print(gw_df.head)
+
+# convert gw dataframe to geodataframe
+gw_gdf = gpd.GeoDataFrame(gw_df, geometry=gpd.points_from_xy(gw_df['lon'], gw_df['lat']), crs='EPSG:4326')
+# print(gw_gdf.head)
+
+groups = gw_gdf.groupby('well_id')
+
+gw_gdf['gw_vel'] = np.nan
+gw_gdf['gw_vel_unc'] = np.nan
+
+for wid, sub in groups:
+    sub = sub.sort_values('date')
+    gw_ts = sub['obs_gw'].values.astype(float)
+    gw_x  = (sub['date'] - sub['date'].min()).dt.days.values.astype(float)
+
+    gw_model = calc_wls(gw_x, gw_ts)
+    gw_vel_day, gw_unc_day = gw_model.params[1], gw_model.bse[1]
+
+    # convert gw values from m/day to m/year
+    gw_vel = gw_vel_day * 365.25
+    gw_unc = gw_unc_day * 365.25
+
+    # append results in gw_gdf
+    mask = gw_gdf['well_id'] == wid
+    gw_gdf.loc[mask, 'gw_vel'] = gw_vel
+    gw_gdf.loc[mask, 'gw_vel_unc'] = gw_unc
+
+# Open vu tif
+vu_tif = OpenTif(IN_VU)
+
+# extract vu value at well location
+vals = gw_gdf.geometry.apply(lambda pt: vu_tif.extract_pixel_value(pt.x, pt.y, 8))
+gw_gdf[['vu', 'vu_sd']] = pd.DataFrame(vals.tolist(), index=gw_gdf.index)
+
+# print to check
+print(gw_gdf[['well_id','lon','lat', 'vu','vu_sd']].drop_duplicates().head(10))
+
+# plot 
+gw_vel = gw_gdf['gw_vel'].values.astype(float)
+cum_vel = gw_gdf['vu'].values.astype(float)
+
+rel_model = calc_wls(cum_vel, gw_vel)
+
+if rel_model is not None:
+    c = rel_model.params[0]  # intercept
+    b = rel_model.params[1]  # slope
+    # c_unc = rel_model.bse[0]
+    # b_unc = rel_model.bse[1]
+
+print(f'gw_vel = {b:.4f} * vu + {c:.4f}')
+
+plt.figure(figsize=(8, 8), dpi=120)
+sns.scatterplot(data=gw_gdf, x='vu', y='gw_vel', color='#6cabeb', edgecolor='dimgray', linewidth=0.4, s=50, alpha=0.35)
+
+x_line = np.linspace(gw_gdf['vu'].min(), gw_gdf['vu'].max(), 100)
+X_line = sm.add_constant(x_line)
+y_line = rel_model.predict(X_line)
+
+label_text = f"WLS fit: GW change rate = {b:.4f} * VU + {c:.4f}"
+plt.plot(x_line, y_line, color='darkred', linewidth=1.8, label=label_text)
+plt.legend(fontsize=12)
+
+plt.xlabel('Vertical velocity (mm/yr)', fontsize=12)
+plt.ylabel('groundwater change rate (m/yr)', fontsize=12)
+plt.title('VU vs groundwater Change Rate', fontsize=14)
+
+# save plot
+out_vel_plot = os.path.join(BASE_DIR, 'outputs', 'GW_vs_JohnVU_.png')
+plt.tight_layout()
+plt.savefig(out_vel_plot)
+plt.close()
+print(f'GW vs Cum velocity scatter saved to {out_vel_plot}.')
