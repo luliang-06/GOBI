e2f7451b95b78dce73ee970b6fcb6339c89d55bb
luliang-06 <s2593484@forth03.geos.ed.ac.uk>
2026-03-28 18:03:50 +0000

    env name setting update to avoid collide with setting in yml

--- init_env.sh
+++ init_env.sh
@@ -2,6 +2,11 @@
 
 ENV_NAME="gobi"
 
+if [[ "${BASH_SOURCE[0]}" == "${0}" ]]; then
+    echo "Please run: source init_env.sh"
+    exit 1
+fi
+
 # check conda
 if ! command -v conda >/dev/null 2>&1; then
     echo "Error: conda not found. Please install Miniconda or Anaconda first."
@@ -11,7 +16,7 @@ fi
 # if env not exist, then create
 if ! conda env list | awk '{print $1}' | grep -qx "$ENV_NAME"; then
     echo "Creating conda environment '$ENV_NAME'..."
-    conda env create -f environment_essential.yml
+    conda env create -f environment_essential.yml -n "$ENV_NAME"
 else
     echo "Conda environment '$ENV_NAME' already exists, skipping creation."
 fi
