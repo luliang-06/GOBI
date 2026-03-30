95bf839fc0c4e30d35066b1a9fbeec3db8762b27
luliang-06 <s2593484@forth03.geos.ed.ac.uk>
2026-03-30 19:01:07 +0100

    input paths edited to relative path

--- scripts/gmt_plot_points_on_raster.sh
+++ scripts/gmt_plot_points_on_raster.sh
@@ -11,16 +11,21 @@ gmt set MAP_LABEL_OFFSET auto
 gmt set MAP_TITLE_OFFSET 2p
 gmt set MAP_ANNOT_OFFSET_SECONDARY auto
 
+BASE_DIR=$(cd "$(dirname "$0")/../.." && pwd)
+points="${BASE_DIR}/outputs_test/GWLcr_VU_ModelResult.csv"
+vu_shiyang="${BASE_DIR}/outputs_test/gwl_cr_SYref.nc"
+vu_shiyang_all="${BASE_DIR}/outputs_test/gwl_cr_SYref_all.nc"
+OUT_DIR="${BASE_DIR}/outputs_test"
 
-points=/exports/geos.ed.ac.uk/comet/lliang/GOBI_proj/outputs/GWLcr_VU_ModelResult.csv # or a separate txt file (if comma delimited, you'll need to change the awk statement below slightly)
-gps=/exports/geos.ed.ac.uk/comet/lliang/GOBI_proj/data/GPS_merge.csv
+# points=/exports/geos.ed.ac.uk/comet/lliang/GOBI_proj/data/GWLcr_VU_ModelResult.csv # or a separate txt file (if comma delimited, you'll need to change the awk statement below slightly)
+# gps=/exports/geos.ed.ac.uk/comet/lliang/GOBI_proj/data/GPS_merge.csv
 
-raster=/exports/geos.ed.ac.uk/comet/lliang/GOBI_proj/data/gwl_cr_all.nc
-vu_shiyang=/exports/geos.ed.ac.uk/comet/lliang/GOBI_proj/outputs/gwl_cr_SYref.nc
-vu_AHB=/exports/geos.ed.ac.uk/comet/lliang/GOBI_proj/data/vu_AHB.nc
-vu_ref=/exports/geos.ed.ac.uk/comet/lliang/GOBI_proj/outputs/gps_ref/vu_shiyang_referenced.nc
+# raster=/exports/geos.ed.ac.uk/comet/lliang/GOBI_proj/data/gwl_cr_all.nc
+# vu_shiyang=/exports/geos.ed.ac.uk/comet/lliang/GOBI_proj/outputs/gwl_cr_SYref.nc
+# vu_AHB=/exports/geos.ed.ac.uk/comet/lliang/GOBI_proj/data/vu_AHB.nc
+# vu_ref=/exports/geos.ed.ac.uk/comet/lliang/GOBI_proj/outputs/gps_ref/vu_shiyang_referenced.nc
 
-out_dir=/exports/geos.ed.ac.uk/comet/lliang/GOBI_proj/outputs/
+# out_dir=/exports/geos.ed.ac.uk/comet/lliang/GOBI_proj/outputs/
 
 
 # # plot GPS on vu
@@ -38,6 +43,16 @@ gmt begin ${out_dir}/gwlcr_on_vu png
 	gmt basemap -R101.7/104.7/37.3/39.3 -JX6i -B1 -BWeSn+t"Observed GWL change rate & InSAR Prediction"							# -R: map extent ｜ -J: projection | -B: axis interval | -B: map frame (capital: label & ticks)
     gmt makecpt -Croma -T-1/1 -I 													# change the limits of your colour map
 	gmt grdimage $vu_shiyang -n+c -Q 												    # -n: interpolation | +c: clip | -Q: set nans as transparent
+    gmt colorbar -DjTL+w1.8/13%+o1/1+h+e+ml -F+gwhite+p0.1p -Bx1+l"@~D@~GWL/@~D@~t" -By+l"m/yr"						# change the 3 in -Bx3 for tick label interval on the colourbar
+	awk -F "," '(NR>1){print $3, $4, $13}' $points | gmt plot -Sc0.22 -C -W0.4p,black
+gmt end
+
+
+# plot GWL change rate  on vu_all
+gmt begin ${out_dir}/gwlcr_on_vuall png
+	gmt basemap -R101.7/104.7/37.3/39.3 -JX6i -B1 -BWeSn+t"Observed GWL change rate & InSAR Prediction"							# -R: map extent ｜ -J: projection | -B: axis interval | -B: map frame (capital: label & ticks)
+    gmt makecpt -Croma -T-1/1 -I 													# change the limits of your colour map
+	gmt grdimage $vu_shiyang_all -n+c -Q 												    # -n: interpolation | +c: clip | -Q: set nans as transparent
     gmt colorbar -DjTL+w1.8/13%+o1/1+h+e+ml -F+gwhite+p0.1p -Bx1+l"@~D@~GWL/@~D@~t" -By+l"m/yr"						# change the 3 in -Bx3 for tick label interval on the colourbar
 	awk -F "," '(NR>1){print $3, $4, $13}' $points | gmt plot -Sc0.22 -C -W0.4p,black
 gmt end
